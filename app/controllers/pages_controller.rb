3c718c563ac0b25d86b8118d080c5b30816f02c8
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2014-01-31 14:12:13 -0500

    final styling and images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,15 +16,6 @@ class PagesController < ApplicationController
 		@donation = Donation.find(params[:id])
 	end
 	def thank
-		@donations = Donation.all
-		@goal = 100000
-		@amounts = []
-		@donations.each do |d|
-			if d.completed == true
-				@amounts.push(d.amount)
-			end
-		end
-		@perc = (@amounts.sum.to_f/@goal)*100
 	end
 	def help
 	end
